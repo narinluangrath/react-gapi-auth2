953fea09a23f78a09f08b35eccd71172a032eba4
Narin Luangrath <narin.luangrath@gmail.com>
2021-04-11 19:14:51 -0700

    some boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  // A set of global variables that need to be available in all test environments
+  // globals: {},
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // moduleNameMapper: {},
+  // The root directory that Jest should scan for tests and modules within
+  // rootDir: undefined,
+};
