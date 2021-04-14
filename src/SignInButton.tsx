bdd26f244c950eaa03eb8365c513d982a2997b96
Narin Luangrath <narin.luangrath@gmail.com>
2021-04-14 00:13:19 -0700

    add SignInButton

---
+++ src/SignInButton.tsx
@@ -0,0 +1,33 @@
+// https://github.com/typescript-eslint/typescript-eslint/issues/2540
+// eslint-disable-next-line no-use-before-define
+import React, { useEffect } from "react";
+
+import { useGoogleAuthContext } from "./GoogleAuthProvider";
+
+type SignInButtonProps = {
+  id?: string;
+  options?: {
+    scope?: string;
+    width?: number;
+    height?: number;
+    longtitle?: boolean;
+    theme?: "light" | "dark";
+    onsuccess?(googleUser: gapi.auth2.GoogleUser): void;
+    onfailure?(reason: { error: string }): void;
+  };
+};
+
+export const SignInButton = (props: SignInButtonProps) => {
+  const { id = "react-gapi-auth2-sign-in-button", options = {} } = props;
+  const context = useGoogleAuthContext();
+
+  if (!context) {
+    throw Error("Must use `SignInButton` inside of `GoogleAuthProvider`");
+  }
+
+  useEffect(() => {
+    gapi.signin2.render(id, options);
+  }, [id, options]);
+
+  return <button id={id} />;
+};
